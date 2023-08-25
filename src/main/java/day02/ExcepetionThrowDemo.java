cb51015783a7c30c30ad60618e28fab900dc094e
Shayla White <shayladwhite1@gmail.com>
2023-08-25 08:27:10 -0700

    day2

---
+++ src/main/java/day02/ExcepetionThrowDemo.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class ExceptionThrowDemo {
+    public static void divideAndAssignValue(int[] arr, int numerator, int denominator, int index){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
